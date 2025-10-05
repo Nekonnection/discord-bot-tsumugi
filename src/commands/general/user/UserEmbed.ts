4b6b023140b1c79ae43881d29b79c4d210133a25
NekosanQ <NekosanQ@outlook.com>
2025-10-05 17:23:47 +0900

    fix: ユーザー情報のEmbedにボットの絵文字設定を追加

--- src/commands/general/user/UserEmbed.ts
+++ src/commands/general/user/UserEmbed.ts
@@ -1,6 +1,7 @@
 import { ChatInputCommandInteraction, EmbedBuilder, GuildMember, User } from 'discord.js';
 
 import { EmbedFactory } from '../../../factories/EmbedFactory.js';
+import { config } from '../../../utils/config.js';
 import { dateTimeFormatter } from '../../../utils/dateTimeFormatter.js';
 import { PermissionTranslator } from '../../../utils/PermissionTranslator.js';
 import { statusAddEmoji } from '../../../utils/statusEmojiAdd.js';
@@ -25,10 +26,12 @@ class UserEmbed {
         const permissionBitfield = member.permissions.bitfield;
         const permissions = new PermissionTranslator(permissionBitfield).permissionNames;
 
+        const botEmoji = member.user.bot ? config.botEmoji : '';
         const userName = `ユーザー名(ID): ${user.username} (${user.id})`;
         const userGlobalName = `表示名: ${user.globalName ?? 'なし'}`;
         const userStatus = statusAddEmoji(member.presence?.status ?? 'offline');
         const accountCreationDate = `アカウント作成日: ${dateTimeFormatter(user.createdAt)}`;
+
         const guildJoinDate = `サーバー参加日: ${member.joinedAt ? dateTimeFormatter(member.joinedAt) : '不明'}`;
         const memberNickname = `ニックネーム: ${member.nickname ?? 'なし'}`;
         const memberPermissions = permissions.length > 0 ? permissions.map((name) => `\`${name}\``).join(', ') : 'なし';
@@ -38,7 +41,7 @@ class UserEmbed {
 
         const embed = this.embedFactory
             .createBaseEmbed(interaction.user)
-            .setTitle(`ユーザー情報`)
+            .setTitle(`ユーザー情報 ${botEmoji}`)
             .setFields(
                 { name: '基本情報', value: basicInfo },
                 { name: 'メンバー情報', value: memberInfo },
