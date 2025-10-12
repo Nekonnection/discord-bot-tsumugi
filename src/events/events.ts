22402178e78782c4cc34c0a0e5fa44693569e99d
NekosanQ <NekosanQ@outlook.com>
2025-10-12 18:15:13 +0900

    feat: guildCreateイベントが発火した際の処理を追加し、EmbedFactoryにシステム用Embedメソッドを実装

--- src/events/events.ts
+++ src/events/events.ts
@@ -1,7 +1,8 @@
 import readyEvent from './ClientReadyEvent.js';
+import guildCreate from './GuildCreateEvent.js';
 import interactionCreateEvent from './InteractionCreateEvent.js';
 import messageCreate from './MessageCreateEvent.js';
 
-const events = [readyEvent, interactionCreateEvent, messageCreate];
+const events = [readyEvent, guildCreate, interactionCreateEvent, messageCreate];
 
 export default events;
