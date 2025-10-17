79519443ea020b38489dbb1e66107556977637a8
NekosanQ <NekosanQ@outlook.com>
2025-10-17 16:58:13 +0900

    feat:CustomSlashSubcommandBuilderにクールダウンプロパティを追加

--- src/utils/CustomSlashSubCommandBuilder.ts
+++ src/utils/CustomSlashSubCommandBuilder.ts
@@ -3,10 +3,16 @@ import { SlashCommandSubcommandBuilder } from 'discord.js';
 class CustomSlashSubcommandBuilder extends SlashCommandSubcommandBuilder {
     public category?: string;
     public usage?: string;
+    public cooldown?: number;
     public defaultBotPermissions?: string;
 
     public constructor() {
         super();
+    }
+
+    public setCooldown(seconds: number): this {
+        this.cooldown = seconds;
+        return this;
     }
 
     public setCategory(category: string): this {
